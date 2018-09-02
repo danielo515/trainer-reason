d91ab2d1f37b9283b8d64696cb857f41f20aef8d
Danielo Rodriguez <rdanielo@gmail.com>
2018-09-02 12:21:12 +0200

    feat(build): add a publish step and improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,36 @@ const outputDir = path.join(__dirname, "build/");
 const isProd = process.env.NODE_ENV === 'production';
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
 
 module.exports = {
   entry: './src/Index.bs.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
-    publicPath: outputDir,
-    filename: 'Index.js',
+    // publicPath: outputDir,
+    filename: '[name].[chunkhash].js',
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          'css-loader',
-          'sass-loader'
-        ]
-      })
-    }]
+    rules: 
+    [
+        { test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [ 'css-loader', 'sass-loader' ]
+          })
+        }
+    ]
   },
   plugins: [
+    new ExtractTextPlugin('css/styles.css'),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
+
     }),
-    new ExtractTextPlugin('css/styles.css'),
+    new CleanWebpackPlugin(["build"], {
+      watch: true
+    })
   ]
 };
